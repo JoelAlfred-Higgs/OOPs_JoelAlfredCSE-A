9821bec57ef7665d06fbd01e4fc995319e6555c4
25CS074 <25CS074@licet.com>
2026-07-09 15:24:13 +0530

    Initial commit

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = input.nextInt();
+         int fact =1 ,i;
+         for(i=1;i<=n;i++){
+            fact*=i ;
+         }
+         System.out.println("Factorial of "+n+" is "+fact );
+    
+    }
+}
