d7fc459ef14874c9c218f579181ea464e38f1cdf
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-07-12 22:05:42 +0530

    Initial commit

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fibonacci {
+    void main(){
+    int i,n,a=0,b=1,c;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number:");
+    
+    n = sc.nextInt();
+    for(i=0;i<n;i++){
+        System.out.print(a+" ");
+        c = b+a;
+        a= b;
+        b = c;
+    }
+    sc.close();
+}
+
+
+}
