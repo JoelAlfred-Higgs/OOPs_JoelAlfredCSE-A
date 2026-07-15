1f37f1e8a9e3489ae8d5805348b38d94cc1a1c4e
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-07-15 22:07:21 +0530

    initial commit

---
+++ LeapYear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LeapYear {
+    public static void main(String [] args){
+       int year;
+       Scanner obj = new Scanner(System.in);
+       System.out.print("Enter Year:");
+       year = obj.nextInt();
+       if((year%4==0&&year%100!=0)||(year%400==0))
+        System.out.print("Its a leapyear");
+       else
+        System.out.print("Its not a leap year");
+       obj.close();
+    }
+}
