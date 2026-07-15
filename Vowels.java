ccef2ea519dff6f19b90a38a5e6993ee4e6e565c
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-07-15 21:55:56 +0530

    initial commit

---
+++ Vowels.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Vowels {
+    public static void main(String[] args){
+        char alpha,arr[] = {'A','a','E','e','I','i','O','o','U','u'};
+        Scanner obj = new Scanner(System.in);
+        int flag = 0;
+        System.err.print("Enter a Character: ");
+        alpha = obj.next().charAt(0);
+        for(int i = 0;i<10;i++){
+            if(arr[i]==alpha){
+                System.out.print("Its a Vowel");
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0){
+        System.err.print("Its not a vowel");
+    }
+
+        obj.close();
+    }
+    
+}
