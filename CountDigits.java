56b1b0cccd57f81e7980b759ef3a50a530e68cd9
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-07-22 22:41:35 +0530

    Initial commit

--- CountDigits.java
+++ CountDigits.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class CountDigits {
     public static void main(String [] args){
-       int num,count = 0,dig;
+       int num,count = 0;
        Scanner obj = new Scanner(System.in);
        System.out.print("Enter a Number: ");
        num = obj.nextInt();
        while(num!=0){
-        dig = num%10;
         count++;
         num = num/10;
        }
