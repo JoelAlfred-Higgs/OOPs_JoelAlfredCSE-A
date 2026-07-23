886060a8ab24bfca2dea85c1c90bee339bf85bbf
25CS074 <25CS074@licet.com>
2026-07-23 14:26:23 +0530

    Initial commit

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class SelectionSort{
+   public static void main(String[] args) {
+      int arr[] = new int[10],i,j,minindex,temp;
+      Scanner obj = new Scanner (System.in);
+      System.out.print("Enter Array elements: ");
+      for(i=0;i<5;i++){
+        arr[i] = obj.nextInt();
+      }
+      for(i=0;i<5;i++){
+        minindex = i;
+        for(j = i+1;j<5;j++){
+            if(arr[j]<arr[minindex])
+                minindex = j;
+        }
+        // swap i and minindex elements
+        temp = arr[i];
+        arr[i] = arr[minindex];
+        arr[minindex] = temp;
+      }
+      // sorted array
+      System.out.println("Sorted Array using Selection Sort");
+      for(i=0;i<5;i++)
+        System.out.print(arr[i] + " ");
+      obj.close();
+   } 
+}
