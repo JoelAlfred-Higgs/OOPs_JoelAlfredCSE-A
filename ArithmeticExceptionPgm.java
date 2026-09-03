8838602a88462ab2b8d9e2afbb6a019468c7d88a
Joel_Alfred <joelalfred.29csa@licet.ac.in>
2026-09-03 14:33:23 +0530

    Add files via upload

---
+++ ArithmeticExceptionPgm.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArithmeticExceptionPgm{
+    public static void main(String[] args) {
+        int a,b,res;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        try {
+            a = obj.nextInt(); // input exception
+            b = obj.nextInt();
+            res = a/b; // division by zero exception
+            System.out.println("Result: "+res);
+        } catch (ArithmeticException e) {
+            System.out.println("Error message: "+e.getMessage());
+        }
+        catch(InputMismatchException e){
+            System.out.println("Error message: Invalid input, input must be integer!");
+        }
+        finally{
+           obj.close();
+        }
+        
+    }
+}
